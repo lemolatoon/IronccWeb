9c8741aff51c7bdff7feccbb4d6b080c269ac863
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-11-12 17:09:26 +0900

    Add wasm functions

--- ironcc-wasm/src/lib.rs
+++ ironcc-wasm/src/lib.rs
@@ -16,3 +16,18 @@ pub fn add(a: i32, b: i32) -> i32 {
 pub fn preprocess_and_compile(input: String) -> String {
     crate::ironcc::preprocess_and_compile(input).map_or_else(|e| format!("{:?}", e), |s| s)
 }
+
+#[wasm_bindgen]
+pub fn preprocess(input: String) -> String {
+    crate::ironcc::preprocessed_source(input).map_or_else(|e| format!("{:?}", e), |s| s)
+}
+
+#[wasm_bindgen]
+pub fn ast(input: String) -> String {
+    crate::ironcc::parsed_ast(input).map_or_else(|e| format!("{:?}", e), |s| format!("{:?}", s))
+}
+
+#[wasm_bindgen]
+pub fn converted_ast(input: String) -> String {
+    crate::ironcc::converted_ast(input).map_or_else(|e| format!("{:?}", e), |s| format!("{:?}", s))
+}
