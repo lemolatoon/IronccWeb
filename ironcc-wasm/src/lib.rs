a0d1f5d889a3998c2a2909bce6f1f1738b3b3bf8
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-04-11 15:34:55 +0900

    Add catch_unwind

--- ironcc-wasm/src/lib.rs
+++ ironcc-wasm/src/lib.rs
@@ -1,4 +1,5 @@
 extern crate ironcc;
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -14,20 +15,38 @@ pub fn add(a: i32, b: i32) -> i32 {
 
 #[wasm_bindgen]
 pub fn preprocess_and_compile(input: String) -> String {
-    crate::ironcc::preprocess_and_compile(input).map_or_else(|e| format!("{:?}", e), |s| s)
+    let result = panic::catch_unwind(|| {
+        crate::ironcc::preprocess_and_compile(input).map_or_else(|e| format!("{:?}", e), |s| s)
+    });
+    alert(&format!("{:?}", result));
+    result.unwrap_or_else(|_| String::from("Rust compiler panic!"))
 }
 
 #[wasm_bindgen]
 pub fn preprocess(input: String) -> String {
-    crate::ironcc::preprocessed_source(input).map_or_else(|e| format!("{:?}", e), |s| s)
+    let result = panic::catch_unwind(|| {
+        crate::ironcc::preprocessed_source(input).map_or_else(|e| format!("{:?}", e), |s| s)
+    });
+    alert(&format!("{:?}", result));
+    result.unwrap_or_else(|_| String::from("Rust compiler panic!"))
 }
 
 #[wasm_bindgen]
 pub fn ast(input: String) -> String {
-    crate::ironcc::parsed_ast(input).map_or_else(|e| format!("{:?}", e), |s| format!("{:#?}", s))
+    let result = panic::catch_unwind(|| {
+        crate::ironcc::parsed_ast(input)
+            .map_or_else(|e| format!("{:?}", e), |s| format!("{:#?}", s))
+    });
+    alert(&format!("{:?}", result));
+    result.unwrap_or_else(|_| String::from("Rust compiler panic!"))
 }
 
 #[wasm_bindgen]
 pub fn converted_ast(input: String) -> String {
-    crate::ironcc::converted_ast(input).map_or_else(|e| format!("{:?}", e), |s| format!("{:#?}", s))
+    let result = panic::catch_unwind(|| {
+        crate::ironcc::converted_ast(input)
+            .map_or_else(|e| format!("{:?}", e), |s| format!("{:#?}", s))
+    });
+    alert(&format!("{:?}", result));
+    result.unwrap_or_else(|_| String::from("Rust compiler panic!"))
 }
